e779a5daaa7a5de094a20e4fece5147111af1250
heinsoeoo <heinsoeoo.hs@gmail.com>
2025-03-31 16:42:48 +0700

    header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Header from "./layouts/header"
+
 export default function Home() {
     return (
-      <>
-        <h1>Hello, world!</h1>
-        <p>Welcome to my Next.js app!</p>
-        <p>This is a simple example of a Next.js page.</p>
-      </>
+      <div className="w-screen">
+        <Header />
+        <div className="flex flex-col items-center justify-center min-h-screen mx-[250px] py-2">
+          <div className="flex items-center justify-center w-full max-w-4xl px-4 text-center">
+            <h1>Hello, world!</h1>
+            <p>Welcome to my Next.js app!</p>
+            <p>This is a simple example of a Next.js page.</p>
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
     );
 }
---
+++ src/components/navItem.tsx
@@ -0,0 +1,7 @@
+export default function NavItem({ item }: { item: {name: string; color: string } }) {    
+    return (
+        <div className={`items-center cursor-pointer w-[5.3em] h-[18px] font-[200]`}>
+            <span className={`hover:text-[#EB662B] text-[13px] ${item.color}`}>{item.name}</span>
+        </div>
+    )
+}
